0bf67a1742a59de40d739775f807271fbcdf0a4f
zhangnannan626 <znn626@yeah.net>
2020-03-25 14:32:45 +0800

    first commit

---
+++ src/main/java/com/znn/test/MathUtils.java
@@ -0,0 +1,8 @@
+package com.znn.test;
+
+public class MathUtils {
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+}
---
+++ src/test/java/com/znn/test/MathUtilsTest.java
@@ -0,0 +1,41 @@
+package com.znn.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class MathUtilsTest {
+	private int numberA;
+	private int numberB;
+	private int expected;
+
+	//parameters pass via this constructor
+	public MathUtilsTest(int numberA, int numberB, int expected) {
+		this.numberA = numberA;
+		this.numberB = numberB;
+		this.expected = expected;
+	}
+
+	//Declares parameters here
+	@Parameters
+	public static Iterable<Object[]> data1() {
+		return Arrays.asList(new Object[][] { 
+			{ 1, 1, 2 }, 
+			{ 2, 2, 4 }, 
+			{ 8, 2, 11 }, 
+			{ 4, 5, 9 } 
+		});
+	}
+
+	@Test
+	public void test_add() {	
+		assertEquals(expected,MathUtils.add(numberA, numberB));
+	}
+
+}
